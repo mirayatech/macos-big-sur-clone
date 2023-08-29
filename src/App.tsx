29821eb3535115b21545de2099a0e65f186b4578
〽️iraya <mirayaabrodi@gmail.com>
2023-08-29 09:33:42 +0200

    add: implemented styled Container and Navbar components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { styled } from "styled-components";
+import { Navbar } from "./components";
+
 export default function App() {
-  return <div>MacOS Clone 🍏</div>;
+  const Container = styled.div`
+    height: 100vh;
+    background: url("/wallpaper/mojave.jpeg") no-repeat center center/cover;
+  `;
+
+  return (
+    <Container>
+      <Navbar />
+    </Container>
+  );
 }
