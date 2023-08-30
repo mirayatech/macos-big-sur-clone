8d02c2a917c351d4e376f65b46e55ab8544eb65e
〽️iraya <mirayaabrodi@gmail.com>
2023-08-30 08:56:11 +0200

    feat: Add state management for 'setSettingOpen' and app click handling

--- src/components/Dock/Dock.tsx
+++ src/components/Dock/Dock.tsx
@@ -2,17 +2,33 @@ import { useMotionValue } from "framer-motion";
 import { DockApp } from "../../library/constant";
 import { AppIcon } from "./AppIcon";
 import { DockWrapper } from "./Style";
+import { useStore } from "../../library/useStore";
 
 export function Dock() {
   const mouseX = useMotionValue(Infinity);
 
+  const { setSettingOpen } = useStore();
+
+  const onSettingsClick = () => {
+    setSettingOpen(true);
+  };
+
+  const onAppClick = () => {
+    console.log("hello");
+  };
   return (
     <DockWrapper
       onMouseMove={(event) => mouseX.set(event.pageX)}
       onMouseLeave={() => mouseX.set(Infinity)}
     >
       {DockApp.map((item, index) => (
-        <AppIcon mouseX={mouseX} key={index} item={item} />
+        <AppIcon
+          mouseX={mouseX}
+          key={index}
+          item={item}
+          onSettingsClick={onSettingsClick}
+          onAppClick={onAppClick}
+        />
       ))}
     </DockWrapper>
   );
