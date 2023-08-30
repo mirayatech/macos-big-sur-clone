d38c6ac22d0547050aabfe4b913e2a656ce27681
〽️iraya <mirayaabrodi@gmail.com>
2023-08-30 10:21:01 +0200

    feat: smooth background animation while backgrund is changing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
+import { useState, useEffect } from "react";
 import { styled } from "styled-components";
+import { motion } from "framer-motion";
 import { Navbar, Dock, Setting } from "./components";
 import { useStore } from "./library/useStore";
 
 export default function App() {
   const { isSettingOpen, wallpaper } = useStore();
 
+  const [background, setBackground] = useState(wallpaper);
+
+  useEffect(() => {
+    setBackground(wallpaper);
+  }, [wallpaper]);
+
   const Container = styled.div`
     height: 100vh;
     overflow: hidden;
-    background: url(${wallpaper}) no-repeat center center/cover;
+    position: relative;
+  `;
+
+  const Background = styled(motion.div)`
+    z-index: -1;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: url(${background}) no-repeat center center/cover;
   `;
 
   const DockWrapper = styled.div`
@@ -21,11 +39,18 @@ export default function App() {
   return (
     <Container>
       <Navbar />
-      {isSettingOpen && <Setting />}
+
+      {isSettingOpen && <Setting key="setting" />}
 
       <DockWrapper>
         <Dock />
       </DockWrapper>
+
+      <Background
+        initial={{ opacity: 0.85 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0.85 }}
+      />
     </Container>
   );
 }
